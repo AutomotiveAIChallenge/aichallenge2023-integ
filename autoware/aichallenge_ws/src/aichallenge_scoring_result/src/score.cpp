1b1c689162ff503ec0a4fb1ee295d7277678d828
和正 崎山 <kazsakinotwen@gmail.com>
2023-06-21 13:25:12 +0900

    Sync

--- autoware/aichallenge_ws/src/aichallenge_scoring_result/src/score.cpp
+++ autoware/aichallenge_ws/src/aichallenge_scoring_result/src/score.cpp
@@ -25,18 +25,38 @@ private:
   {
     if (has_published_result)
       return;
-      
+    
+    // const auto is_completed_scoring = msg.is_outside_lane || msg.is_timeout || msg.has_exceeded_speed_limit || msg.has_finished_task3;
+    // if (!is_completed_scoring)
+    //   return;
+
     std::cout << "Scoring completed." << std::endl;
     writeResultJson(msg);
 
     has_published_result = true;
   }
 
+  float calculateDistanceScore(const aichallenge_scoring_msgs::msg::Score& score_msg) {
+    if (score_msg.has_exceeded_speed_limit)
+      return 0.0f;
+
+    auto distance_score = score_msg.distance_score;
+    auto penalty_ratio = 1.0f;
+
+    if (!score_msg.has_finished_task1)
+      penalty_ratio -= 0.05f;
+
+    return distance_score;
+  }
+
   void writeResultJson(const aichallenge_scoring_msgs::msg::Score& score_msg) {
-    std::ofstream ofs("/result.json");
+    float distance_score = 0.0f;
+
+
+    std::ofstream ofs("/aichallenge/result.json");
     ofs << "{" << std::endl;
     ofs << "  \"rawDistanceScore\": " << score_msg.distance_score << "," << std::endl;
-    ofs << "  \"distanceScore\": " << score_msg.distance_score << "," << std::endl;
+    ofs << "  \"distanceScore\": " << calculateDistanceScore(score_msg) << "," << std::endl;
     ofs << "  \"task3Duration\": " << score_msg.task3_duration << "," << std::endl;
     ofs << "  \"isOutsideLane\": " << score_msg.is_outside_lane << "," << std::endl;
     ofs << "  \"isStopped\": " << score_msg.is_stopped << "," << std::endl;
